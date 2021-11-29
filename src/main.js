d951e7baf4d48a2951a00b95ea06738a01e44a22
BesovMusic <besovmusic@yandex.com>
2021-11-29 23:26:00 +0500

    Очистил директорию от лишнего, подключил bootstrap

--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
+
 createApp(App).use(store).mount('#app')
